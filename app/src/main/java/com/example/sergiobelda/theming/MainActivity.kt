40fabf0449f9431dcbde0d0493792bd3eec08ca1
Sergio Belda Galbis <serbelga@inf.upv.es>
2018-11-24 00:37:03 +0100

    Added Components

--- app/src/main/java/com/example/sergiobelda/theming/MainActivity.kt
+++ app/src/main/java/com/example/sergiobelda/theming/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.sergiobelda.theming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.RoundedCornerTreatment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MaterialShapeDrawable().apply {
+            val treatment = RoundedCornerTreatment(24.0f)
+            shapeAppearanceModel.setAllCorners(treatment)
+        }
     }
 }
